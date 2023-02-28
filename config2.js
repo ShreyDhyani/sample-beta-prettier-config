3dee102bbb300f09f91bc3c15cda19163c3142d2
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-02-28 09:34:18 +0530

    Test method 1

---
+++ config2.js
@@ -0,0 +1,29 @@
+const config2 = {
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  overrides: [
+    {
+      files: ["*.html", "*.ts", "*.js"],
+      options: {
+        semi: true,
+        singleQuote: true,
+        trailingComma: "all",
+        tabWidth: 4,
+        printWidth: 100,
+        arrowParens: "avoid",
+        jsxSingleQuote: true,
+        jsxBracketSameLine: false,
+        endOfLine: "auto",
+      },
+    },
+    {
+      files: "*.vue",
+      options: {
+        semi: true,
+      },
+    },
+  ],
+};
+
+export { config2 };
---
+++ index.js
@@ -0,0 +1,4 @@
+import { config1 } from "./config1.js";
+import { config2 } from "./config2";
+
+export default { config1, config2 };
